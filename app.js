1d61532415c979f9001ce940e9b697efebb0f203
Tith moue <62829639+Mouetith@users.noreply.github.com>
2021-09-30 15:56:06 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-function myTest() { 
-                var f, l, c, s, e, result;
+function myTest() {
+                var f, l, c, s, g,b,result;
                 f = document.testf.fname.value;
                 l = document.testf.lname.value;
                 sx = document.testf.sex.value;
                 c = document.testf.class.value;
                 s = document.testf.shift.value;
+                
 
-                result = "នាមខ្លួន=>" + f + "\n";
-                result += "នាមត្រកូល=>" + l + "\n";
+
+                result = "ឈ្មោះ=>" + l +" "+f+ "\n";
                 result += "ភេទ=>" + sx + "\n";
                 result += "ថ្នាក់=>" + c + "\n";
                 result += "វេន=>" + s + "\n";
